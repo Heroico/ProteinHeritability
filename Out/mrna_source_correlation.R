16a02b9c33da92ff0d33f8f6354fd9af443c2809
Numa <alvarobarbeira@gmail.com>
2015-05-27 18:05:05 -0300

    Output of dataframe with correlation, pvalue and samples of affymetrix-predixcan correlation

--- Out/mrna_source_correlation.R
+++ Out/mrna_source_correlation.R
@@ -1,11 +1,13 @@
 
-mrna_source_correlation <- function(file_1, file_2) {
+mrna_source_data <- function(file_1, file_2) {
 	data_1 = read.csv(file_1)
 	data_2 = read.csv(file_2)
 
 	genes <- names(data_1)
 
 	correlation = numeric(length(genes))
+	samples = numeric(length(genes))
+	pvalues = numeric(length(genes))
 	index = 1
 	for( i in genes ) {
 		x <- data_1[[i]]
@@ -14,12 +16,19 @@ mrna_source_correlation <- function(file_1, file_2) {
 		count <- length(OK[OK == TRUE])
 		if (count > 6) {
 			res = cor.test(x, y, alternative="two.sided", method="pearson",conf.level=0.95)
+			pvalues[index] = res$p.value
 			correlation[index] = res$estimate
 		} else {
 			correlation[index] = NA
 		}
+		samples[index] = count
 		index = index+1
 	}
+	results <- data.frame(Gene = genes, Correlation = correlation, P.Value = pvalues, Samples = samples)
+}
+
+mrna_source_correlation <- function(data) {
+	correlation = data$Correlation
 
 	ordered_data <- correlation[order(correlation)]
 	png(filename="predi_affy_by_gene.png",width=720,height=960)
@@ -28,5 +37,7 @@ mrna_source_correlation <- function(file_1, file_2) {
 	result = t.test(correlation, mu=0, conf.level=0.95)
 }
 
-result = mrna_source_correlation("../IntermediateC/intersection_mrna_affy.csv", "../IntermediateC/intersection_mrna_predi.csv")
+data = mrna_source_data("../IntermediateC/intersection_mrna_affy.csv", "../IntermediateC/intersection_mrna_predi.csv")
+write.table(data, "predixcan_affymetrix.table")
+result = mrna_source_correlation(data)
 print(result)
